57f0d9f643cefee72a2d76d923b6f8b64f2316de
Pearce Kieser <5055971+Pearcekieser@users.noreply.github.com>
2023-12-23 01:09:26 -0500

    CW-insights script - add updateURL

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Pearce's Tampermonkey scripts
 
-[Cloudwatch logs UUID helper](): Add's link to a UUID in your results that jumps to a new query for for that UUID. 
+[Cloudwatch logs UUID helper](https://github.com/Pearcekieser/tampermonkey-scripts/raw/main/aws/cw-insights/cw-lambda-deep-dive.js): Add's link to a UUID in your results that jumps to a new query for for that UUID. 
 
 
--- aws/cw-insights/cw-lambda-deep-dive.js
+++ aws/cw-insights/cw-lambda-deep-dive.js
@@ -7,6 +7,8 @@
 // @match        https://*.console.aws.amazon.com/cloudwatch/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=amazon.com
 // @require      https://cdn.jsdelivr.net/npm/dayjs@1/dayjs.min.js
+// @updateURL    https://github.com/Pearcekieser/tampermonkey-scripts/raw/main/aws/cw-insights/cw-lambda-deep-dive.js
+// @downloadURL  https://github.com/Pearcekieser/tampermonkey-scripts/raw/main/aws/cw-insights/cw-lambda-deep-dive.js
 // @grant        none
 // ==/UserScript==
 
